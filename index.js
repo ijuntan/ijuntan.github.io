85ec9d49e76f3bf147300263db50024841bcc6c4
Junior Tanaya <juniortanaya@gmail.com>
2020-06-10 17:00:41 +0700

    link

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import './src/index.css';
+import App from './src/App';
+import * as serviceWorker from './src/serviceWorker';
 
 ReactDOM.render(
   <BrowserRouter>

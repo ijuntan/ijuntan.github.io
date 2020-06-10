dcb61d53c43bcaaa71e25e34a6c886615b1ddcab
Junior Tanaya <juniortanaya@gmail.com>
2020-06-10 17:51:47 +0700

    Updates

---
+++ precache-manifest.5746b32f8f5f047c78635e8746bf0fd9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "079264c77a92b1ed38f4b05084b4eed6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dc014004a1938b968bbf",
+    "url": "/static/css/main.685dea8f.chunk.css"
+  },
+  {
+    "revision": "43f5e7a42599977de91a",
+    "url": "/static/js/2.a1bc70d7.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.a1bc70d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc014004a1938b968bbf",
+    "url": "/static/js/main.75a0a55a.chunk.js"
+  },
+  {
+    "revision": "a1cbec206847193e980a",
+    "url": "/static/js/runtime-main.0d655397.js"
+  }
+]);
